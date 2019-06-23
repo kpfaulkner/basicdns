b8a12d3c9d03307ec1fcbf952f564a1446ad5720
Ken Faulkner <ken.faulkner@gmail.com>
2019-06-23 17:33:04 +1000

    decoding of query section of UDP packet works

--- main.go
+++ main.go
@@ -27,9 +27,12 @@ limitations under the License.
 
 import (
 	"basicdns/models"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	log "github.com/golang/glog"
 	"net"
+	"strings"
 	"sync"
 )
 
@@ -44,7 +47,6 @@ const (
 // This maintains goroutine pool for incoming requests amongst other things.
 type BasicDNS struct {
   wg sync.WaitGroup
-
   requestChannel chan models.RawDNSRequest // requests to go routines will come through the channel.
 
   numResolverGoRoutines int
@@ -56,15 +58,98 @@ func NewBasicDNS(poolSize int ) (*BasicDNS, error) {
   b.numResolverGoRoutines = poolSize
 
 	// channel size of 1000.....  need to figure out what is the best size here.
-	b.requestChannel :=  make(chan models.RawDNSRequest, 1000)
+	requests := make(chan models.RawDNSRequest, 1000)
+	b.requestChannel = requests
 
 	return &b, nil
 }
 
-func processDNSRequest(request []byte, conn *net.UDPConn, client *net.UDPAddr) {
+// readQueryName reads the domain names from the question section.
+func readQueryName(requestBuffer *bytes.Buffer) (string, error) {
 
+	var domainSegments []string
+
+	completed := false
+	for completed == false {
+
+		// read first byte. This will be how long the query domain will be.
+		domainLength, err := requestBuffer.ReadByte()
+
+		if domainLength == 0 {
+			// finito.
+			completed = true
+			continue
+		}
+
+		byteSlice := make([]byte, domainLength)
+
+		_, err = requestBuffer.Read(byteSlice)
+		if err != nil {
+			log.Errorf("Unable to read part of request domain %s\n", err)
+			return "", err
+		}
+		domainSegments = append(domainSegments, string(byteSlice))
+	}
+
+	return strings.Join(domainSegments, "."), nil
 }
 
+// readRequest converts the bytearray to the DNSRequest format.
+func readRequest( requestBuffer *bytes.Buffer) (*models.DNSRequest, error) {
+
+	var header models.DNSHeader
+	err := binary.Read(requestBuffer, binary.BigEndian, &header)
+	if err != nil {
+		log.Errorf("Cannot decode header %s\n", err)
+		log.Errorf("Will not process this request\n") // what should we return to the caller?  Any appropriate error code?
+		
+		return nil, err
+	}
+
+	// loop through and read all record requests.
+	for i:=uint16(0); i< header.QDCount; i++ {
+		name,err  := readQueryName(requestBuffer)
+		if err != nil {
+			log.Errorf("error while reading domain %s\n", err)
+		}
+		log.Infof("got name %s\n", name)
+	}
+
+	return nil, nil
+}
+
+
+// processDNSRequest is where the work happens.
+// Reads the incoming request channel, decodes the request, processes, then returns reponse.
+func processDNSRequest(conn *net.UDPConn, requestChannel chan models.RawDNSRequest) {
+
+	for {
+		request, ok := <-requestChannel
+		if !ok {
+			// closed.....  time to leave!!
+			return
+		}
+
+		var requestBuffer = bytes.NewBuffer(request.RawBytes)
+
+		dnsRequest, err := readRequest( requestBuffer)
+		if err != nil {
+			// unable to process request.... so we respond to the caller?
+			// continue for moment... TODO(kpfaulkner) figure out what to do in case of error!
+			continue
+		}
+
+		log.Infof("dnsRequest is %v\n", dnsRequest)
+
+	}
+}
+
+// createHandlerPool will create a pool of go routines that handle the incoming requests.
+func (b BasicDNS) createHandlerPool( conn *net.UDPConn ) {
+	for i:=0;i< b.numResolverGoRoutines ;i++ {
+    go processDNSRequest( conn, b.requestChannel )
+	}
+}
 
 // RunServer is the main loop for the DNS server.
 // accept connections and passes off to go routines for processing.
@@ -82,6 +167,9 @@ func (b BasicDNS) RunServer() {
 
 	defer conn.Close()
 
+	// About to add numResolverGoRoutines added.
+	b.wg.Add( b.numResolverGoRoutines)
+	b.createHandlerPool(conn)
 
 	// request coming in. Allocated bytes up front.
 	// reading request from main thread.... will this be a bottleneck?
@@ -98,16 +186,21 @@ func (b BasicDNS) RunServer() {
 
 		request := models.RawDNSRequest{ requestBuffer, clientAddr}
 		b.requestChannel <- request
-		// go handleDNSClient(requestBytes, serverConn, clientAddr) // array is value type (call-by-value), i.e. copied
+
 	}
+
+	// although shouldn't leave the for loop above, make sure wg is handled properly.
+	b.wg.Done()
 
 }
 
 func main() {
 
-	// parse args.
+	dns, err := NewBasicDNS(10)
 
-	// run main.
+	if err != nil {
+		log.Errorf("error %s\n", err)
+	}
 
-
+	dns.RunServer()
 }
--- models/models.go
+++ models/models.go
@@ -5,6 +5,22 @@ import "net"
 // RawDNSRequest is the raw information from the socket.
 // This includes the byte array and the client details of the request (so we know where to respond to).
 type RawDNSRequest struct {
-	rawBytes []byte
-	clientAddr *net.UDPAddr
+	RawBytes []byte
+	ClientAddr *net.UDPAddr
+}
+
+
+type DNSRequest struct {
+
+}
+
+// DNSHeader... first 12 bytes of the UDP packet.
+// See https://routley.io/tech/2017/12/28/hand-writing-dns-messages.html for great explanation about this!
+type DNSHeader struct {
+	ID         uint16
+	MiscFlags  uint16
+	QDCount    uint16
+	ANCount    uint16
+	NSCount    uint16
+	ADCount    uint16
 }
