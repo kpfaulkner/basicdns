7e83b88404c0d310f98c2039381a9213866fc857
Ken Faulkner <ken.faulkner@gmail.com>
2019-06-23 19:01:13 +1000

    adding QType and QClass reading

--- models/models.go
+++ models/models.go
@@ -2,6 +2,31 @@ package models
 
 import "net"
 
+type QType uint16
+type QClass uint16
+
+const (
+
+	// QType
+	ARecord QType = 1
+  NameServer QType = 2
+  CName QType = 5
+  SOA QType = 6
+  WKS QType = 11
+  PTR QType = 12
+  HInfo QType = 13
+  MInfo QType = 14
+  MX QType = 15
+  TXT QType = 16
+
+  // QClass
+  IN QClass = 1
+  CH QClass = 3
+  HS QClass = 4
+
+)
+
+
 // RawDNSRequest is the raw information from the socket.
 // This includes the byte array and the client details of the request (so we know where to respond to).
 type RawDNSRequest struct {
@@ -10,8 +35,10 @@ type RawDNSRequest struct {
 }
 
 
+// DNSRequest is DNS Request (duh).
+// Within a single request there can actually be multiple domains to lookup.
 type DNSRequest struct {
-
+	QuerySlice []DNSQuery
 }
 
 // DNSHeader... first 12 bytes of the UDP packet.
@@ -23,4 +50,10 @@ type DNSHeader struct {
 	ANCount    uint16
 	NSCount    uint16
 	ADCount    uint16
+}
+
+type DNSQuery struct {
+	Domain string
+	QT QType
+	QC QClass
 }
